71220b3055579caa2b0f033fb79d8c56417bc0f4
chouheiwa <849131492@qq.com>
2018-08-08 11:01:07 +0800

    Initial Commit

---
+++ BitsharesWallet/BitsharesWallet.h
@@ -0,0 +1,19 @@
+//
+//  BitsharesWallet.h
+//  BitsharesWallet
+//
+//  Created by flh on 2018/8/8.
+//  Copyright © 2018年 flh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BitsharesWallet.
+FOUNDATION_EXPORT double BitsharesWalletVersionNumber;
+
+//! Project version string for BitsharesWallet.
+FOUNDATION_EXPORT const unsigned char BitsharesWalletVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BitsharesWallet/PublicHeader.h>
+
+
